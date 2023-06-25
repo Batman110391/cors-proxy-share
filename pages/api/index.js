1aa01c9ec95b94e0274237ac0ee29c6e0a550036
Simone Gattinara <simone.gattinara@smc.it>
2023-06-25 14:01:26 +0200

    fixed

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,20 +1,49 @@
-const axios = require('axios');
+const fetch = require("node-fetch");
 
-export default function handler(req, res, next) {
-  async function getURI(url) {
-    try {
-      const response = await axios.get(url);
-      console.log(response)
-      if (response.status !== 200) {
-        return res.status(response.status).json({ type: 'error', message: response.statusText });
-      } else {
-      res.json(response.data);
-      }
-    } catch (error) {
-      console.log(error)
-      return res.status(500).json({ type: 'error', message: error.message });
+export default async function handler(req, res) {
+  const url = new URL(req.query["url"]);
+  for (let i in req.query) {
+    if (i !== "url") {
+      url.searchParams.append(i, req.query[i]);
     }
   }
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  getURI(req.body['my-url'])
+
+  const cookie_string = req.headers.cookie || "";
+  const useragent = req.headers["user-agent"] || "";
+
+  const header_to_send = {
+    Cookie: cookie_string,
+    "User-Agent": useragent,
+    "content-type": "application/json",
+    accept: "*/*",
+    host: url.host,
+  };
+
+  const options = {
+    method: req.method.toUpperCase(),
+    headers: header_to_send,
+    body: req.body,
+  };
+
+  if (
+    req.method.toUpperCase() === "GET" ||
+    req.method.toUpperCase() === "HEAD"
+  ) {
+    delete options.body;
+  }
+
+  const response = await fetch(url, options);
+  const response_text = await response.text();
+  const headers = response.headers.raw();
+
+  let cookie_header = null;
+  if (headers["set-cookie"]) {
+    cookie_header = headers["set-cookie"];
+  }
+
+  res.writeHead(response.status, {
+    "content-type": String(headers["content-type"]) || "text/plain",
+    "set-cookie": cookie_header || [],
+  });
+  res.end(response_text);
 }
