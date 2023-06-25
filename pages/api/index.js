a5e49551a6d58f3bcb60a1cee4855203d624b3af
Simone Gattinara <simone.gattinara@smc.it>
2023-06-25 14:54:21 +0200

    fixed

--- pages/api/index.js
+++ pages/api/index.js
@@ -2,51 +2,13 @@ const axios = require("axios");
 
 export default async function handler(req, res) {
   const url = req.query["url"];
-  const params = {};
-  for (let i in req.query) {
-    if (i !== "url") {
-      params[i] = req.query[i];
-    }
-  }
 
-  const cookie_string = req.headers.cookie || "";
-  const useragent = req.headers["user-agent"] || "";
-
-  const headers_to_send = {
-    Cookie: cookie_string,
-    "User-Agent": useragent,
-    "content-type": "application/json",
-    accept: "*/*",
-  };
-
-  const options = {
-    method: req.method.toUpperCase(),
-    headers: headers_to_send,
-    data: req.body,
-    params: params,
-  };
-
-  if (
-    req.method.toUpperCase() === "GET" ||
-    req.method.toUpperCase() === "HEAD"
-  ) {
-    delete options.data;
-  }
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
   try {
-    const response = await axios(url, options);
-    const headers = response.headers;
+    const response = await axios.get(url);
 
-    let cookie_header = null;
-    if (headers["set-cookie"]) {
-      cookie_header = headers["set-cookie"];
-    }
-
-    res.writeHead(response.status, {
-      "content-type": headers["content-type"] || "text/plain",
-      "set-cookie": cookie_header || [],
-    });
-    res.end(response.data);
+    res.status(200).send(response.data);
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal Server Error");
